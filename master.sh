cbc4c3b4510daf328e2924aa0e8dc4efaea160f8
dhtt <dhthutrang@gmail.com>
2022-06-02 14:59:14 +0200

    Add checkIfDirExit

--- master.sh
+++ master.sh
@@ -1,3 +1,14 @@
+checkIfDirExist(){
+    allPaths=("$@")
+    for Path_ in "${allPaths[@]}"
+    do
+        if [ ! -d $Path_ ] 
+            then
+                echo 'Please check if input folders are named '$Path_
+                exit 9999
+            fi
+    done
+    
 # Parse arguments for input path, output path
 while getopts 'i:' flag
 do 
@@ -17,7 +28,8 @@ RNAseq_SAMPLESHEET=$mRNA_seq_PATH'/samplesheet.csv'
 RNAseq_OUTPUT=$mRNA_seq_PATH'/rnaseq_output';
 
 ChIP_seq_PATH=$INPUT_PATH'/ChIP_seq';
-# TODO: Error if the folders containing fastq are not mRNA_seq and ChIP_seq
+allPaths=( $mRNA_seq_PATH $ChIP_seq_PATH )
+checkIfDirExist "${allPaths[@]}"
 
 # $nfcore_rnaseq='/home/dhthutrang/tools/nf-core/rnaseq/bin'
 echo 'Create output folder'
@@ -26,4 +38,4 @@ echo 'Make sample sheet'
 python $nfcore_rnaseq/fastq_dir_to_samplesheet.py $mRNA_seq_PATH $RNAseq_SAMPLESHEET -st unstranded -r1 '_1.fastq.gz' -r2 '_2.fastq.gz';
 echo 'Run nf-core/rna_seq'
 nextflow run nf-core/rnaseq --input $RNAseq_SAMPLESHEET --outdir $RNAseq_OUTPUT --genome GRCh38 -profile docker
-echo 'Finished.'
+echo 'Finished.',
