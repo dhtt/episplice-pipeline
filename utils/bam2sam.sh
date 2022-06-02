70514ae2cc5ccc1a09ec02ba520335d797194ba0
dhtt <dhthutrang@gmail.com>
2022-06-02 18:31:24 +0200

    Add bam2sam.sh

---
+++ utils/bam2sam.sh
@@ -0,0 +1,29 @@
+# Parse arguments for input path, output path
+while getopts 'i:o:' flag
+do 
+    case "${flag}" in 
+        (i) INPUT_PATH=${OPTARG};;
+        (o) OUTPUT_PATH=${OPTARG};;
+        (:) 
+            case ${OPTARG} in 
+                (i) exit 9999;;
+                (o) exit 9999;;
+            esac;;
+    esac
+done
+
+echo "Converting BAM to SAM"
+bam_to_sam(){
+    ACC_NO=${f%%.*}
+    ACC_NO=${ACC_NO##*/}
+    echo "Converting $ACC_NO"
+    (samtools sort -n -O SAM $f > $OUTPUT_PATH/$ACC_NO.sam) || echo "Err: Cannot convert BAM to SAM $ACC_NO"
+}
+
+for f in $INPUT_PATH/*.bam
+do
+    bam_to_sam $f &
+done
+wait
+
+echo "Finished converting BAM to SAM"
