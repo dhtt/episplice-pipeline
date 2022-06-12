4c14d8c9b3259a2d910bc811aec19292ee1bbf92
dhtt <dhthutrang@gmail.com>
2022-06-12 12:30:27 +0200

    Test get_dexseq_plot() from utils.R

--- DEU_scripts/main_script.R
+++ DEU_scripts/main_script.R
@@ -10,6 +10,15 @@ default_epigenome2 <- "MCF7_50nM"
 default_referencegenome <- "/home/dhthutrang/ENCODE/refgen/reference_genome.2021_.corrected.gtf"
 # source("/home/dhthutrang/Krebs/episplice-pipeline/DEU_scripts/DEXSeq_analysis.R")
 
-# DEXSeq_analysis 2 ####
+# DEXSeq analysis report ####
+# source("/home/dhthutrang/Krebs/episplice-pipeline/DEU_scripts/utils.R")
+# deu_genes_list <- get_deu_genes()
+# summarize_deu_genes_info()
 
-source("/home/dhthutrang/Krebs/episplice-pipeline/DEU_scripts/utils.R")
+# DEXSeq analysis report ####
+default_epigenome1 <- NULL
+default_epigenome2 <- NULL
+default_gene_id <- NULL
+source("/home/dhthutrang/Krebs/episplice-pipeline/DEU_scripts/utils.R", encoding = "UTF-8")
+get_dexseq_plot(gene_id = "STIM1")
+get_dexseq_plot(epi_id1 = "MCF7_DMSO", epi_id2 = "MCF7_50nM", gene_id = "STIM1")
