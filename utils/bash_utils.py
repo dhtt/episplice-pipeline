5c6c24c883fccd4a3c3a6290489bcc89f384bf4c
dhtt <dhthutrang@gmail.com>
2022-08-26 14:42:59 +0200

    Modify & clean up PREPARE PEAK BED FILES

--- utils/bash_utils.py
+++ utils/bash_utils.py
@@ -13,23 +13,44 @@ def duplicate_file(current_path, new_path):
         shell=True
         )
 
-def merge_bed_file(input_dir: str, output_dir: str, bed_files: list, merged_bed_name: str):
-    merged_bed_file_name = join(output_dir, merged_bed_name + '.bed')
-    sorted_bed_file_name = join(output_dir, merged_bed_name + '.sorted.bed')
-    bed_files = [join(input_dir, s) for s in bed_files]
-    print(bed_files)
+def sort_bed_file(bed_file):
+    subprocess.call(
+        "sort -k1,1 -k2,2n %s > %s"
+        %(bed_file, bed_file + ".sorted"),
+        shell=True
+        )
+    subprocess.call(
+        "mv %s %s"
+        %(bed_file + ".sorted", bed_file),
+        shell=True
+        )
 
+def rm_file(file):
+    subprocess.call(
+        "rm %s" %(file),
+        shell=True
+        )
+
+def merge_bed_file(input_dir: str, output_dir: str, bed_files: list, merged_bed_name: str):
+    # Define input and outname
+    merged_bed_file_name = join(output_dir, merged_bed_name + '.bed')
+    bed_files = [join(input_dir, s) for s in bed_files]
+
+    # Join bed files by cat
     subprocess.call(
         "cat %s > %s" %(' '.join(bed_files), merged_bed_file_name),
         shell=True
     )
+    
+    # Sort bed files by bedtools
+    sort_bed_file(merged_bed_file_name)
     subprocess.call(
-        "sort -k1,1 -k2,2n %s > %s"
-        %(merged_bed_file_name, sorted_bed_file_name),
+        "bedtools merge -c 5 -o max -i %s > %s"
+        %(merged_bed_file_name, merged_bed_file_name + ".merged"),
         shell=True
         )
     subprocess.call(
-        "bedtools merge -c 7 -o max -i %s > %s && rm %s"
-        %(sorted_bed_file_name, merged_bed_file_name, sorted_bed_file_name),
+        "mv %s %s"
+        %(merged_bed_file_name + ".merged", merged_bed_file_name),
         shell=True
         )
